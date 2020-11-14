402e97f2ab0a2862b7f1c111cc2b5fbf315ee95f
Jean-Poul <cph-jl360@cphbusiness.dk>
2020-11-14 11:55:21 +0100

    refactoring

--- src/main/java/rest/CartoonResource.java
+++ src/main/java/rest/CartoonResource.java
@@ -1,10 +1,10 @@
-
 package rest;
 
 import com.google.gson.Gson;
 import dto.Rick_MortyDTO;
 
 import java.io.IOException;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -14,25 +14,26 @@ import utils.HttpUtils;
 
 @Path("cartoon")
 public class CartoonResource {
-    
+
     @Path("first")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
     public String getCartoon() throws IOException {
         Gson gson = new Gson();
-        
+
         String cartoon = HttpUtils.fetchData("https://rickandmortyapi.com/api/character/1");
         System.out.println(cartoon + " CARTOON");
-        
+
         Rick_MortyDTO cartoonDTO = gson.fromJson(cartoon, Rick_MortyDTO.class);
         System.out.println(cartoonDTO.toString() + " DTO");
-        
+
         String cartoonJSON = gson.toJson(cartoonDTO);
         System.out.println(cartoonJSON + " JSON");
-        
+
         return cartoonJSON;
     }
-    
+
     @Path("/{id}")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
@@ -40,14 +41,14 @@ public class CartoonResource {
         Gson gson = new Gson();
         String cartoon = HttpUtils.fetchData("https://rickandmortyapi.com/api/character/" + id);
         System.out.println(cartoon + " CARTOON");
-        
+
         Rick_MortyDTO cartoonDTO = gson.fromJson(cartoon, Rick_MortyDTO.class);
         System.out.println(cartoonDTO.toString() + " DTO");
-        
+
         String cartoonJSON = gson.toJson(cartoonDTO);
         System.out.println(cartoonJSON + " JSON");
-        
+
         return cartoonJSON;
     }
-    
+
 }
